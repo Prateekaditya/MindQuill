674c76a134e58dd3bfb32aa7deabcf7b0ed0752b
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-06-23 22:10:58 +0530

    added blogs

---
+++ src/app/blogs/page.jsx
@@ -0,0 +1,97 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const blogItems = [
+    {
+        "id":"1",
+        "title":"In the window of time",
+        "excerpt":"The times which make you remember the past",
+        "image":"/windowboy.jpeg",
+        "url":"/demo-slug"
+    },
+    {
+        "id":"2",
+        "title":"In the window of time",
+        "excerpt":"The times which make you remember the past",
+        "image":"/garden.jpeg",
+        "url":"/demo-slug"
+    },
+    {
+        "id":"3",
+        "title":"In the window of time",
+        "excerpt":"The times which make you remember the past",
+        "image":"/windowboy.jpeg",
+        "url":"/demo-slug"
+    },
+    {
+        "id":"1",
+        "title":"In the window of time",
+        "excerpt":"The times which make you remember the past",
+        "image":"/windowboy.jpeg",
+        "url":"/demo-slug"
+    },
+    {
+        "id":"2",
+        "title":"In the window of time",
+        "excerpt":"The times which make you remember the past",
+        "image":"/garden.jpeg",
+        "url":"/demo-slug"
+    },
+    {
+        "id":"3",
+        "title":"In the window of time",
+        "excerpt":"The times which make you remember the past",
+        "image":"/windowboy.jpeg",
+        "url":"/demo-slug"
+    },
+    {
+        "id":"1",
+        "title":"In the window of time",
+        "excerpt":"The times which make you remember the past",
+        "image":"/windowboy.jpeg",
+        "url":"/demo-slug"
+    },
+    {
+        "id":"2",
+        "title":"In the window of time",
+        "excerpt":"The times which make you remember the past",
+        "image":"/garden.jpeg",
+        "url":"/demo-slug"
+    },
+    {
+        "id":"3",
+        "title":"In the window of time",
+        "excerpt":"The times which make you remember the past",
+        "image":"/windowboy.jpeg",
+        "url":"/demo-slug"
+    },
+]
+
+export default function Blogs(){
+    return(
+        <section>
+            <div className="grid gap-12 sm:grid-cols-2  lg:grid-cols-3 p-8">
+                {blogItems.map((blog,index)=>(
+                    <BlogCard key={index} 
+                              title={blog.title}
+                              image={blog.image}
+                              excerpt={blog.excerpt}
+                              url={blog.url}
+                              />
+                ))}
+            </div>
+        </section>
+    )
+}
+
+const BlogCard= ({title,excerpt,image,url})=>{
+    return(
+        <div className="flex flex-col bg-gray-600/20  p-2 border gap-2 hover:scale-105 transition-all delay-100 duration-200">
+            <div className="w-full h-72">
+            <Image className="w-full" src={image} alt={title} width={300} height={150}/></div>
+            <p className="text-xl font-bold">{title}</p>
+            <p className="text-sm text-zinc-400 ">{excerpt}</p>
+            <Link className="bg-zinc-500/30 rounded text-xs border w-fit px-2 py-1" href={`/blog/${url}`}>Read More</Link>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Brain, CalendarDays, FilePen, Lightbulb } from "lucide-react";
+import { Brain, CalendarDays, FilePen, Icon, Layers, Lightbulb, PenTool, Zap } from "lucide-react";
 import Image from "next/image";
 
 
@@ -9,7 +9,7 @@ export default function Home() {
   return (
       <div>
         <section>
-          <div className="mb-29 flex flex-col items-center  justify-center h-[90vh] gap-6 sm:h-[80vh]">
+          <div className=" flex flex-col items-center  justify-center h-[70vh] gap-6 sm:h-[80vh]">
             <h1 className="text-xl font-bold sm:text-3xl md:text-4xl lg:text-5xl">Where Content, Memories, and Life Converge</h1>
             <p className="text-[12px] sm:text-[14px] md:text-[17px] text-zinc-600">Transform how you create, manage, and relive your entire digital life.
             </p>
@@ -19,9 +19,29 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section>MIDDLE</section>
         <section>
-          <div className=" flex flex-col items-center  h-[90vh] gap-4 sm:h-screen">
+          <div className="flex mb-30 bg-gray-600/20 md:h-[50vh] w-full justify-center ">
+            <div className="flex flex-col items-center justify-center cursor-pointer p-10">
+              <span className="hover:scale-110 duration-150"> <PenTool size={50} /> </span>
+              <h2 className="text-xl sm:text-2xl md:text-3xl">Intuitive Editor</h2>
+              <p className="text-[12px] text-center  sm:text-[14px] md:text-[16px] text-zinc-500">Create and edit content with user friendly interface</p>
+            </div>
+            <div className="flex flex-col items-center justify-center  cursor-pointer p-10">
+              <span className="hover:scale-110 duration-150"> <Layers size={50}/> </span>
+              <h2 className="text-xl sm:text-2xl md:text-3xl">Seamless interface</h2>
+              <p className="text-[12px] text-center sm:text-[14px] md:text-[16px] text-zinc-500">Create and edit content with user friendly interface</p>
+            </div>
+            <div className="flex flex-col items-center justify-center  cursor-pointer  p-10">
+              <span className="hover:scale-110 duration-150">
+                <Zap size={50}/>
+              </span>
+              <h2 className="text-xl sm:text-2xl md:text-3xl">Lightning Fast</h2>
+              <p className="text-[12px] text-center  sm:text-[14px] md:text-[16px] text-zinc-500">Create and edit content with user friendly interface</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className=" flex flex-col items-center  h-[170vh] gap-4 sm:h-screen">
           <h1 className="text-xl font-bold sm:text-3xl  md:text-4xl lg:text-5xl">What You Can Store and Manage</h1>
             <div className="flex  flex-wrap gap-9 justify-center md:w-250 p-4 md:gap-30 mt-8 ">
                 <div className="p-4 shadow-2xl shadow-slate-500/15 h-50 justify-center gap-2 w- flex flex-col items-center border-2 w-100 hover:scale-110 duration-150">
@@ -63,8 +83,15 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <secton>
-          ENDINg
+        <secton  >
+          <div className="flex bg-gray-600/20 flex-col sm:mt-20 items-center justify-center h-[60vh] gap-[19px] sm:h-[50vh] ">
+            <h4 className="text-2xl md:text-3xl font-bold">Ready to Unlock Your Creative Genius?</h4>
+            <p className="text-sm md:text-[16px] text-zinc-500">Be among successful creators worldwide on MindQuill</p>
+            <div className="flex gap-3">
+              <input typr='text' placeholder="Enter Your Email" className="bg-zinc-800/60 px-2 py-[10px] text-sm focus:outline-none rounded text-zinc-500"/>
+              <Button variant='outline'>Submit</Button>
+            </div>
+          </div>
         </secton>
       </div>
   );
