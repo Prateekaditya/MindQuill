6be334e70d0006b5b3e6b6d3936bc8b3934e9248
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-06-25 23:58:52 +0530

    Update page.jsx

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
 export default function Blog(){
     return(
         <div>
-            Hii Your were testing
+            <div>
+                <Image className="w-100 sm:w-150 md:w-180" src="/windowboy.jpeg" alt="title" width={500} height={300}></Image>
+            </div>
+            <h2>In the window of time</h2>
         </div>
     )
 }
